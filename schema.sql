b7fa1f8088af0e225e94322dad16331a9e857f7b
JacobToton <Jake_Toton@live.com>
2024-05-16 18:32:02 -0600

    Created schema.sql and seed.sql.
    Created schema.sql to generate the three tables needed that are outlined in the module description. Created seed.sql. to populate the database and tables with mock data for testing.
    
    Added a call to seed.sql within the schema file to auto populate the data and call a simple join to view the tables.

---
+++ schema.sql
@@ -0,0 +1,48 @@
+-- *Create content manager database, drop if it already exists to sanitize the data.
+DROP DATABASE IF EXISTS content_manager_db;
+CREATE DATABASE content_manager_db;
+
+-- *Connect to content manager database.
+\c content_manager_db;
+
+-- *Create department table.
+-- *Set ID as primary key.
+-- *Add department_name row to the table.
+CREATE TABLE department (
+  id SERIAL PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
+);
+
+-- *Create role table.
+CREATE TABLE role (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER,
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+-- *Create employee table.
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER,
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
+
+-- *Call the seed.sql file to add mock data into the database. --
+\i seed.sql
+
+-- *Query to retrieve employee name, department name, and role. --
+SELECT 
+  employee.first_name, 
+  employee.last_name,
+  department.department_name,
+  role.title
+FROM 
+  employee
+  JOIN role ON employee.role_id = role.id
+  JOIN department ON role.department_id = department.id;
